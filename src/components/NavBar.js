457357305b54ee8369d46ec84976436d5c516d37
Hayden Carson <info@haydencarson.com>
2019-07-29 11:06:17 +1000

    updated to hide the nav buttons earlier and hide the sub headings when the device is too small

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,19 +3,22 @@ import styles from './NavBar.module.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import {Link} from 'react-router-dom'
+import MediaQuery from 'react-responsive'
 
 export default class MyNavBar extends React.Component{
 	render(){
 		return (
-				<Navbar className={styles.navContainer} bg="dark" variant="dark" expand="sm" sticky="top">
+				<Navbar className={styles.navContainer} bg="dark" variant="dark" expand="md" sticky="top">
 					<Navbar.Brand className={styles.navBrand}>
 						<h2 className={styles.whiteText}>
 							Hayden Carson
 						</h2>
 					</Navbar.Brand>
-					<Navbar.Brand className={styles.navBrandSubHeadings}>
-						<p className={styles.whiteText}>Software Engineer</p><p className={styles.whiteText}>Web Developer</p>
-					</Navbar.Brand>
+					<MediaQuery query="(min-width: 600px)">
+						<Navbar.Brand className={styles.navBrandSubHeadings}>
+							<p className={styles.whiteText}>Software Engineer</p><p className={styles.whiteText}>Web Developer</p>
+						</Navbar.Brand>
+					</MediaQuery>
 					<Navbar.Toggle aria-controls="basic-navbar-nav"/>
 					<Navbar.Collapse className="justify-content-end">
 						<Nav className={styles.navLinksContainer}>
