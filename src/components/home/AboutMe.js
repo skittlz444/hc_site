5d0ae7f9c3ce4f68b3cd94f3534b0599eaf015d5
Hayden Carson <info@haydencarson.com>
2019-07-30 17:51:53 +1000

    restyle about me to a more gridlike layout [#167281967]

--- src/components/home/AboutMe.js
+++ src/components/home/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col, Image} from 'react-bootstrap'
+import {Row, Col, Image, Container} from 'react-bootstrap'
 import styles from './AboutMe.module.css'
 import LazyLoad from 'react-lazyload'
 import hero from './img/about_me_background.jpg'
@@ -8,22 +8,38 @@ import hero_tiny from './img/about_me_background_tiny.jpg'
 export default class AboutMe extends React.Component{
 	render(){
 		return(
-				<Row className={styles.aboutMeRow}>
-					<Col lg={{order:1}} className={styles.aboutMeCol}>
-						<Col>
-							<div className={styles.aboutMeText}>
-								<h3 className={styles.whiteText}>
-									Computer Scientist
-								</h3>
-								<h5 className={styles.whiteText}>
-									Designing efficient solutions for complex challenges
-								</h5>
-								
-							</div>
-						</Col>
+				<Row className={styles.aboutContainer}>
+					<Col lg={{order:1}} className={styles.aboutMeCol + " wow fadeInLeft"}>
+						<Container>
+							<Row className={styles.aboutMeRow}>
+								<Col>
+									<div className={styles.aboutMeText}>
+										<h3 className={styles.whiteText}>
+											Computer Scientist
+										</h3>
+										<h5 className={styles.whiteText}>
+											Designing efficient solutions for complex challenges
+										</h5>
+										
+									</div>
+								</Col>
+							</Row>
+							<Row >
+								<Col>
+									<div  className={styles.aboutMeText}>
+										<h3 className={styles.whiteText}>
+											Web Developer
+										</h3>
+										<h5 className={styles.whiteText}>
+											Working with you to build elegant web solutions for your business
+										</h5>
+									</div>
+								</Col>
+							</Row>
+						</Container>
 					</Col>
-					<Col className={styles.aboutMeCol} lg={{order:2}}>
-						<Col>
+					<Col className={styles.aboutMeCol + " " + styles.aboutMeHeroContainer + " wow fadeInRight"} lg={{order:2}}>
+						<Col className={styles.aboutMeHeroContainer}>
 							<div>
 								<LazyLoad
 									placeholder={<Image className={styles.lazy} src={hero_tiny} alt="Hero"/>}
@@ -33,23 +49,12 @@ export default class AboutMe extends React.Component{
 									offsetVertical={300}
 									once={true}
 									>
-										<Image className={styles.heroImage + " wow fadeIn"} src={hero} alt="Hero"/>
+										<Image className={styles.heroImage} src={hero} alt="Hero"/>
 								</LazyLoad>
 							</div>
 						</Col>
 					</Col>
-					<Col className={styles.aboutMeCol} lg={{order:3}}>
-						<Col>
-							<div  className={styles.aboutMeText}>
-								<h3 className={styles.whiteText}>
-									Web Developer
-								</h3>
-								<h5 className={styles.whiteText}>
-									Working with you to build elegant web solutions for your business
-								</h5>
-							</div>
-						</Col>
-					</Col>
+					
 				</Row>
 		)
 	}
