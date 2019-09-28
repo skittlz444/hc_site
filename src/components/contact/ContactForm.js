e86d29a15836a48bd125db6519efc9793b531857
Hayden Carson <info@haydencarson.com>
2019-09-28 12:10:57 +1000

    [#167563007 finishes] finished client side validation error

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import styles from './ContactForm.module.css'
 import {Row, Col, Form, Button, Container} from 'react-bootstrap'
 
 export default class ContactForm extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			errors: null
+		};
+		this.form = React.createRef();
+		this.validate = this.validate.bind(this);
 		this.handleFormSubmit = this.handleFormSubmit.bind(this);
 		
 	}
+
+	validate() {
+		return this.form.current.reportValidity();
+	}
+
 	handleFormSubmit(e){
-		if(e.currentTarget.checkValidity() === false){
+		if(!this.validate()){
 			// alert("INVALID");
+			this.setState({['errors']: "It appears something is wrong with your information, please check your details and ensure everything is filled out and correct."})
 			e.preventDefault();
 			e.stopPropagation();
 		} else{
-			// alert("submitted");
+			alert("submitted");
 			e.preventDefault();
 			e.stopPropagation();
 		}
@@ -23,10 +35,13 @@ export default class ContactForm extends React.Component{
 	render(){
 		return(
 			<Container>
-				<Form>
+				<div className={styles.errors}>
+					{this.state.errors}
+				</div>
+				<Form ref={this.form}>
 					<Row>
 						<InputHalfWidth controlId="contactName" placeholder="Who are you?"/>
-						<InputHalfWidth controlId="contactEmail" placeholder="What is your email?"/>
+						<InputHalfWidth controlId="contactEmail" type="email" placeholder="What is your email?"/>
 						<InputFullWidth controlId="contactSubject" placeholder="How can I help you?"/>
 						<TextAreaFullWidth controlId="contactBody" placeholder="Lets talk specifics."/>
 					</Row>
@@ -57,7 +72,7 @@ const InputFullWidth = (props) => {
 	return(
 		<Col lg={12} xs={12} sm={12} md={12}>
 			<Form.Group controlId={props.controlId}>
-				<Form.Control type="text" required placeholder={props.placeholder}/>
+				<Form.Control type={props.type == null? "text": props.type} required placeholder={props.placeholder}/>
 			</Form.Group>
 		</Col>
 		)
@@ -67,7 +82,7 @@ const InputHalfWidth = (props) => {
 	return(
 		<Col lg={6} xs={12} sm={12} md={6}>
 			<Form.Group controlId={props.controlId}>
-				<Form.Control type="text" required placeholder={props.placeholder}/>
+				<Form.Control type={props.type == null? "text": props.type} required placeholder={props.placeholder}/>
 			</Form.Group>
 		</Col>
 		)
