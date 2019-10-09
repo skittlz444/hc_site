9f47c9747886fc327e0063d780f7d269cf54e480
Hayden Carson <info@haydencarson.com>
2019-10-10 08:16:26 +1100

    [finishes #167282078] finally happy with the validation display

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from './components/shared/Footer';
 import sharedStyles from './components/shared/SharedStyles.module.css'
 import SuspensePlaceholder from './components/shared/placeholders/SuspensePlaceholder.js'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 require('animate.css')
 require('bootstrap')
@@ -17,6 +19,7 @@ const Contact = lazy(() => contactPromise);
 function App() {
   return (
     <div className="App">
+    <ToastContainer/>
       <Router>
         <MyNavBar/>
         <Suspense fallback={<SuspensePlaceholder/>}>
